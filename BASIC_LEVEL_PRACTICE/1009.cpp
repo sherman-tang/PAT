f2e74acaeffac58027b611cf28d782bab5fc47ff
sherman-tang <sherman_tang@163.com>
2018-11-11 06:50:13 -0500

    add description in 1009.cpp

--- BASIC_LEVEL_PRACTICE/1009.cpp
+++ BASIC_LEVEL_PRACTICE/1009.cpp
@@ -4,6 +4,25 @@
 #include <vector>
 using namespace std;
 
+/*
+1009 说反话 
+
+describe:
+给定一句英语，要求你编写程序，将句中所有单词的顺序颠倒输出
+
+input format:
+测试输入包含一个测试用例，在一行内给出总长度不超过 80 的字符串。字符串由若干单词和若干空格组成，其中单词是由英文字母（大小写有区分）组成的字符串，单词之间
+用 1 个空格分开，输入保证句子末尾没有多余的空格
+
+output format:
+每个测试用例的输出占一行，输出倒序后的句子
+
+example:
+input:
+    Hello World Here I Come
+output:
+    Come I Here World Hello
+*/
 int main()
 {
     string str;
